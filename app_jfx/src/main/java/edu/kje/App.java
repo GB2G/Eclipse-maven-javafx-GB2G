2992a44fbea39cd6407ab0fe64d7e3acca8a3255
Kevin_El-Saikali <elskev17@ecolecatholique.ca>
2022-04-26 13:09:29 -0400

    fix???

--- app_jfx/src/main/java/edu/kje/App.java
+++ app_jfx/src/main/java/edu/kje/App.java
@@ -23,7 +23,6 @@ public class App extends Application {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction( new EventHandler<ActionEvent>() {
-
             @Override
             public void handle (ActionEvent event){
                 System.out.println("Hello World!");
